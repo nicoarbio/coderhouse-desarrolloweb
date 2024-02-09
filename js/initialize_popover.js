5d787efec9f963a43fc2db196d86e1e73567a55c
Nicolás G Arbio <nicolasarbio@outlook.com>
2024-02-08 22:08:04 -0300

    Add popovers with GitHub links

---
+++ js/initialize_popover.js
@@ -0,0 +1,5 @@
+// Inicializa todos los popovers en la página
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, { html: true })
+})
